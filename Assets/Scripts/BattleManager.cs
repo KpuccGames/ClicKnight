ca216b5b9d8dcd87728fbb532ef82664bd28864f
Kpucc <kpucc91@gmail.com>
2019-03-11 01:13:59 +0300

    реализация тапов

---
+++ Assets/Scripts/BattleManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleManager : MonoBehaviour
+{
+    //////////////
+    public void OnClick()
+    {
+        // TODO реализация атаки
+        Debug.Log("I'm attacking monster!");
+    }
+}
