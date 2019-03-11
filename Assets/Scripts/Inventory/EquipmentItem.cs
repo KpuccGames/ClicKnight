fac0ecb817c64cc4fe1f7f3aee1cfc0ff145d336
Kpucc <kpucc91@gmail.com>
2019-03-12 02:05:23 +0300

    завел базовые классы для предметов инвентаря

---
+++ Assets/Scripts/Inventory/EquipmentItem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public enum EquipmentSlot
+{
+    helmet = 0,
+    chest = 1,
+    legs = 2,
+    boots = 3,
+    shoulders = 4,
+    gloves = 5,
+    neck = 6,
+    finger = 7
+}
+
+public class EquipmentItem
+{
+    public int AttackBonus { get; private set; }
+    public int DefenseBonus { get; private set; }
+}
