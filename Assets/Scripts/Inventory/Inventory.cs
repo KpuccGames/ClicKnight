cd4575b0d8f45d39a80c37755d4994ba28015b5b
Kpucc <kpucc91@gmail.com>
2019-03-17 23:49:46 +0300

    реализовано отображение предметов в инвентаре и парсинг json

---
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using SimpleJson;
+
+public class Inventory : MonoBehaviour
+{
+    public InventoryCell[] m_InventoryCells;
+
+    ////////////////
+    void Start()
+    {
+        string text = File.ReadAllText("Assets/GameData/test_equip.json");
+        JsonObject json = Helper.ParseJson(text);
+
+        JsonArray array = json.Get<JsonArray>("items");
+
+        for (int i = 0; i < array.Count; i++)
+        {
+            JsonObject itemJson = array.GetAt<JsonObject>(i);
+
+            Sprite icon = Resources.Load<Sprite>((string)itemJson["icon"]);
+
+            m_InventoryCells[i].SetItemIcon(icon);
+        }
+    }
+}
--- Assets/Scripts/Inventory/InventoryCell.cs
+++ Assets/Scripts/Inventory/InventoryCell.cs
@@ -1,8 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryCell : MonoBehaviour
 {
-    
+    public Image m_Icon;
+
+    //////////////
+    public void SetItemIcon(Sprite icon)
+    {
+        m_Icon.overrideSprite = icon;
+    }
 }
