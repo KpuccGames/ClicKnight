ce8742df70ca6b70cb4aafbabbdfc1b5f15c65a3
Ilya Solomoychenko <i.solomoychenko@dpill.games>
2019-06-30 22:18:07 +0300

    поправил множественное создание объектов выбора миссии

--- Assets/Scripts/SelectMissionDialog/SelectMissionDialog.cs
+++ Assets/Scripts/SelectMissionDialog/SelectMissionDialog.cs
@@ -7,6 +7,8 @@ public class SelectMissionDialog : BaseDialog
     [SerializeField] private MissionItem m_MissionPrefab;
     [SerializeField] private RectTransform m_MissionsContainer;
 
+    private bool m_IsInited = false;
+
     //////////////////
     public override void Show()
     {
@@ -18,12 +20,28 @@ public class SelectMissionDialog : BaseDialog
     //////////////////
     private void InitView()
     {
+        if (m_IsInited)
+            return;
+
         List<MissionData> missions = GameDataStorage.Instance.Missions;
+
+        missions.Sort((a, b) =>
+        {
+            if (a.Number > b.Number)
+                return 1;
+            else
+                return -1;
+        });
 
         foreach (MissionData mission in missions)
         {
+            if (mission.Number > PlayerProfile.Instance.NormalWorldMissionNumber) // здесь указываем максимальный уровень миссии в зависимости от мира, в который идем
+                continue;
+
             MissionItem missionItem = Instantiate(m_MissionPrefab, m_MissionsContainer);
             missionItem.Setinfo(mission);
         }
+
+        m_IsInited = true;
     }
 }
